0312dfe17c5579a8757d383db6f12cd3693cc043
Vic <ji3g4vic03@gmail.com>
2022-11-16 12:01:49 +0800

    Initial Commit

---
+++ homework2-copy app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework2-copy app
+//
+//  Created by 王衍智 on 2022/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
